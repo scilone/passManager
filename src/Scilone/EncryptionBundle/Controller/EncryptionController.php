e887862d30aea1471d69f5421891c3cd994af0ac
Polo <nnobre@ccmbenchmark.com>
2017-03-03 18:27:34 +0100

    files forgotten

---
+++ src/Scilone/EncryptionBundle/Controller/EncryptionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Scilone\EncryptionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Scilone\EncryptionBundle\Form\MasterKeyType;
+use Scilone\EncryptionBundle\Entity\MasterKey;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Form;
+
+/**
+ * Class MasterKeyController
+ *
+ * @package Scilone\EncryptionBundle\Controller
+ */
+class EncryptionController extends Controller
+{
+
+    public function xhrDecryptAction(string $salt, string $text)
+    {
+        return new JsonResponse($this->get('scilone_encryption.service')->decrypt($text, $salt));
+    }
+}
